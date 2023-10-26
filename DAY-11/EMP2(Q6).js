bcc258f6602f4de66050e141513257c465f4446d
IsHaBeRa30 <147516070+IsHaBeRa30@users.noreply.github.com>
2023-10-26 18:53:11 +0530

    Add files via upload

---
+++ DAY-11/EMP2(Q6).js
@@ -0,0 +1,9 @@
+
+        var ename ="shruti";
+        var empid ="880965";
+        var job ="analyst";
+        var salary = 50000;
+        //Latest version: string templates (interpolation)
+        var str = `Empno :${empid} 12645,  Ename  :${ename}  Scott,  Job :${job} Manager, Salary :${salary} 45000`;
+
+        console.log(str);
